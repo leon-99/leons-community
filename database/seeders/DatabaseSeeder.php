0d178db5d1f3b379f9bc8c28e40df114f7f52a2f
Win Khant Aung <leonzifer@gmail.com>
2023-06-10 09:58:49 +0630

    June 10

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Comment;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(10)->create();
+        Article::factory()->count(20)->create();
+        Comment::factory()->count(20)->create();
+
+        $list = ["HTML", "CSS", "Laravel", "PHP", "JavaScript"];
+
+        foreach($list as $name) {
+            Category::create(["name" => $name]);
+        }
+    }
+}
